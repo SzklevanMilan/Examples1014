a4b6cde3bf7a1c17433839121b25fd51a43af3b8
SzklevanMilan <szklevanm@gmail.com>
2020-10-14 09:46:24 +0200

    óraimunka

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,22 +39,88 @@ class Program
         //5 alatti számok kiíratása
         var otAlatt = from n in numbers
                       where n < 5 
+                      orderby n descending
                       select n;
+        // azonnali kiértékelés --> átalakítással
+        //var ujTomb = otAlatt.ToList();
+        //var ujTomb = otAlatt.ToArray();
 
+        foreach (var o in otAlatt)
+        {
+            Console.Write($"{o}, ");
+        }
         // Kik laknak "CO" állmaban?
-
+        var coState = from c in customers
+                      where c.State == "CO"
+                      orderby c.Name
+                      select c;
+        Console.WriteLine("\n'CO' államban élők");
+        foreach (var c in coState)
+        {
+            Console.WriteLine($"{c.Name,6}--{c.State}");
+        }
 
         // számok értéke növelve 2-vel
+        var kettovel = from n in numbers
+                       select n + 2;
+        
+        //var kettoTomb = kettovel.ToArray();
+        //for (int i = 0; i < kettoTomb.Length; i++)
+        //{
+        //    Console.WriteLine($"{numbers[i]}-->{kettoTomb[i]}");
+        //}
 
+        int i = 0;
+        Console.WriteLine();
+        foreach (var k in kettovel)
+        {
+            Console.WriteLine($"{numbers[i++]} --> {k}");
+        }
 
         // A Customer-ből csak név és telefonszám kell --> anonim objektum típus felhasználása
+        var namePhone = from c in customers
+                        orderby c.Name
+                        select new { nev = c.Name, tel = c.Phone };
+        //anonim objektum a "new" kulcs szóval adjuk meg
 
+        Console.WriteLine();
+        foreach (var n in namePhone)
+        {
+            Console.WriteLine($"{n.nev,8} {n.tel}");
+        }
 
         // számokból --> szöveg
+        Console.WriteLine();
+        int[] ujtomb = { 2, 4, 0, 1, 9, 6 };
 
+        var szoveges = from u in ujtomb
+                       select numNames[u];
+        foreach (var s in szoveges)
+        {
+            Console.Write(s+" ");
+        }
 
         // csoportosítás 
+        Console.WriteLine();
+        var nevek = from c in customers
+                    select c.Name;
+        var nevLista = nevek.ToList();
+        var kezdoBetu = from n in nevLista
+                        orderby n
+                        where n[0] != 'R'
+                        group n by n[0] into tempNevek
+                        select tempNevek;
 
+
+        Console.WriteLine("\nCsoportosítás kezdőbetű szerint");
+        foreach (var csoport in kezdoBetu)
+        {
+            Console.WriteLine("Kezdőbetű: {0}", csoport.Key);
+            foreach (var csoportTag in csoport)
+            {
+                Console.WriteLine($"\t{csoportTag}");
+            }
+        }
 
 
         Console.ReadLine();
